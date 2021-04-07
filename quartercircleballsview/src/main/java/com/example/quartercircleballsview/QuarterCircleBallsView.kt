1b5a4ded203980fe05ad1ce84170f5b993d0436f
Anwesh43 <anweshthecool0@gmail.com>
2021-04-08 01:03:38 +0530

    adding Int and Float extension methods

--- quartercircleballsview/src/main/java/com/example/quartercircleballsview/QuarterCircleBallsView.kt
+++ quartercircleballsview/src/main/java/com/example/quartercircleballsview/QuarterCircleBallsView.kt
@@ -24,3 +24,8 @@ val scGap : Float = 0.02f / parts
 val strokeFactpr : Float = 90f
 val sizeFactor : Float = 4.8f
 val delay : Long = 20
+
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
